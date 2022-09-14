43acd01b61e0f13cd4e9eb275292569c44184835
o-maklecovs <olegsmaklecovs@gmail.com>
2022-09-14 18:05:14 +0100

    Added exception handling to display manager, partially working

--- src/main/java/com/sparta/Olegs/view/DisplayManager.java
+++ src/main/java/com/sparta/Olegs/view/DisplayManager.java
@@ -4,6 +4,7 @@ import com.sparta.Olegs.controller.SortManager;
 import com.sparta.Olegs.controller.SorterTypes;
 import com.sparta.Olegs.utils.RandArrayBuilder;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DisplayManager {
@@ -14,7 +15,6 @@ public class DisplayManager {
 
     public static DisplayManager getDisplayManager() { return displayManager; }
 
-    // TODO add exception handling
     public void run() {
         System.out.println("==== SORT MANAGER ====");
         Scanner sc = new Scanner(System.in);
@@ -24,28 +24,25 @@ public class DisplayManager {
             System.out.println("1. Bubble sort");
             System.out.println("2. Merge sort");
             System.out.println("3. Binary tree sort");
-            if (sc.hasNextInt()) {
+            try {
                 int algo = sc.nextInt();
+                if (algo < 1 || algo > 3) throw new Exception(); // not working
                 System.out.println();
                 System.out.println("Enter array size: ");
-                if (sc.hasNextInt()) {
-                    int size = sc.nextInt();
-                    RandArrayBuilder arrBuilder = new RandArrayBuilder();
-                    int[] arr = arrBuilder.build(size);
-                    System.out.println();
-                    switch (algo) {
-                        case 1 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.BUBBLE, arr));
-                        case 2 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.MERGE, arr));
-                        case 3 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.BINARY, arr));
-                    }
-                    break;
-                } else {
-                    sc.next();
-                    System.out.println("Invalid input");
-                    System.out.println();
+                int size = sc.nextInt();
+                if (size < 1) throw new Exception(); // not working
+                RandArrayBuilder arrBuilder = new RandArrayBuilder();
+                int[] arr = arrBuilder.build(size);
+                System.out.println();
+                switch (algo) {
+                    case 1 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.BUBBLE, arr));
+                    case 2 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.MERGE, arr));
+                    case 3 -> print(SortManager.getSortManager().getSorterAndSort(SorterTypes.BINARY, arr));
                 }
-            } else {
+                break;
+            } catch (Exception e) {
                 sc.next();
+                System.out.println();
                 System.out.println("Invalid input");
                 System.out.println();
             }
